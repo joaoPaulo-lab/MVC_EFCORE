1a9eedd440a5fecc13fb8e906d7cf7d8fe05dff3
Joaoftaghn <joaopaulosantos.xx@gmail.com>
2021-10-18 15:29:07 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_EFCORE.Models
+{
+    public class Categoria
+    {
+        public Categoria(int id, string nome)
+        {
+            ID = id;
+            Nome = nome;
+        }
+
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public ICollection<Livro> Livros { get; set; }
+    }
+}
---
+++ Models/Livro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MVC_EFCORE.Models
+{
+    public class Livro
+    {
+        public Livro() =>
+            DataEntrada = DateTime.Now;
+        public Livro(int iD, int categoriaID, string titulo, string autor, int quantidade, decimal preco, Categoria categoria) : this()
+        {
+            ID = iD;
+            CategoriaID = categoriaID;
+            Titulo = titulo;
+            Autor = autor;
+            Quantidade = quantidade;
+            Preco = preco;
+            Categoria = categoria;
+        }
+
+        public Livro(int iD, int categoriaID, string titulo, string autor, int quantidade, decimal preco, DateTime dataEntrada, Categoria categoria)
+        {
+            ID = iD;
+            CategoriaID = categoriaID;
+            Titulo = titulo;
+            Autor = autor;
+            Quantidade = quantidade;
+            Preco = preco;
+            DataEntrada = dataEntrada;
+            Categoria = categoria;
+        }
+
+        public int ID { get; set; }
+        public int CategoriaID { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataEntrada { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
